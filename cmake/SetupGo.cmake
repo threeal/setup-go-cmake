f84cf8c39230a100ebb665eadc68a285aeada547
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-22 12:20:15 +0700

    feat: add support for setting up the latest Go on Windows (#9)
    * feat: rename downloaded build file according to URL name
    
    * feat: add support to set up Go on Windows
    
    * ci: modify `test-project` job to also run on Windows

--- cmake/SetupGo.cmake
+++ cmake/SetupGo.cmake
@@ -16,23 +16,32 @@ function(setup_go)
   elseif(CMAKE_SYSTEM_NAME STREQUAL Darwin)
     set(URL https://go.dev/dl/go1.22.2.darwin-amd64.tar.gz)
     set(EXPECTED_MD5 6a8e1186969f0ce1cc6fc2551d834c6b)
+  elseif(CMAKE_SYSTEM_NAME STREQUAL Windows)
+    set(URL https://go.dev/dl/go1.22.2.windows-amd64.zip)
+    set(EXPECTED_MD5 125813601d166b742b5ab86d986be78c)
   else()
     message(FATAL_ERROR "Unsupported system for setting up Go: ${CMAKE_SYSTEM_NAME}")
   endif()
 
   file(MAKE_DIRECTORY ${CMAKE_BUILD_DIR}/_deps)
+  get_filename_component(FILENAME ${URL} NAME)
+
   file(
-    DOWNLOAD ${URL} ${CMAKE_BUILD_DIR}/_deps/go.tar.gz
+    DOWNLOAD ${URL} ${CMAKE_BUILD_DIR}/_deps/${FILENAME}
     EXPECTED_MD5 ${EXPECTED_MD5}
   )
 
   execute_process(
-    COMMAND tar -xf ${CMAKE_BUILD_DIR}/_deps/go.tar.gz -C ${CMAKE_BUILD_DIR}/_deps
+    COMMAND tar -xf ${CMAKE_BUILD_DIR}/_deps/${FILENAME} -C ${CMAKE_BUILD_DIR}/_deps
     RESULT_VARIABLE RES
   )
   if(NOT RES EQUAL 0)
-    message(FATAL_ERROR "Failed to extract '${CMAKE_BUILD_DIR}/_deps/go.tar.gz' to '${CMAKE_BUILD_DIR}/_deps' (${RES})")
+    message(FATAL_ERROR "Failed to extract '${CMAKE_BUILD_DIR}/_deps/${FILENAME}' to '${CMAKE_BUILD_DIR}/_deps' (${RES})")
   endif()
 
-  set(GO_EXECUTABLE ${CMAKE_BUILD_DIR}/_deps/go/bin/go PARENT_SCOPE)
+  if(CMAKE_SYSTEM_NAME STREQUAL Windows)
+    set(GO_EXECUTABLE ${CMAKE_BUILD_DIR}/_deps/go/bin/go.exe PARENT_SCOPE)
+  else()
+    set(GO_EXECUTABLE ${CMAKE_BUILD_DIR}/_deps/go/bin/go PARENT_SCOPE)
+  endif()
 endfunction()
