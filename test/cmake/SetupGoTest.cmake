5dd7e7b36be688129ff0fcc62604e7cd796728a5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-24 15:23:44 +0700

    test: modify test to include module from `find_package`

--- test/cmake/SetupGoTest.cmake
+++ test/cmake/SetupGoTest.cmake
@@ -6,6 +6,7 @@ file(
   EXPECTED_MD5 4ee0e5217b07442d1a31c46e78bb5fac)
 include(${CMAKE_BINARY_DIR}/Assertion.cmake)
 
+find_package(SetupGo REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../../cmake)
 include(SetupGo)
 
 # Asserts whether a Go executable exists with the specified version.
