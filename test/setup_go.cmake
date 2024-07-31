b9873828be9c38945ca3b2223ffb4c6b04f44633
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-31 20:38:33 +0700

    test: organize tests (#71)
    * test: run test cases sequentially in same module
    
    * test: simplify assertion without the need ot `assert_fo_executable` function
    
    * test: shift test declaration to top-level `CMakeLists.txt`
    
    * test: utilize `assertion_add_test` to create CMake test
    
    * ci: run test verbosely

---
+++ test/setup_go.cmake
@@ -0,0 +1,25 @@
+find_package(SetupGo REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)
+
+section("it should set up the latest version of Go")
+  setup_go()
+
+  assert(DEFINED GO_EXECUTABLE)
+  assert(EXISTS "${GO_EXECUTABLE}")
+  assert(IS_EXECUTABLE "${GO_EXECUTABLE}")
+
+  assert_execute_process(
+    COMMAND "${GO_EXECUTABLE}" version
+    OUTPUT "^go version go1.22.5")
+endsection()
+
+section("it should set up a specific version of Go")
+  setup_go(VERSION 1.21.9)
+
+  assert(DEFINED GO_EXECUTABLE)
+  assert(EXISTS "${GO_EXECUTABLE}")
+  assert(IS_EXECUTABLE "${GO_EXECUTABLE}")
+
+  assert_execute_process(
+    COMMAND "${GO_EXECUTABLE}" version
+    OUTPUT "^go version go1.21.9")
+endsection()
