7bb2f656c32c5002788dac7b215c1d361e4ebc69
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-31 20:02:51 +0700

    test: simplify assertion without the need ot `assert_fo_executable` function

--- test/cmake/SetupGoTest.cmake
+++ test/cmake/SetupGoTest.cmake
@@ -8,25 +8,26 @@ include(${CMAKE_BINARY_DIR}/Assertion.cmake)
 
 find_package(SetupGo REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../../cmake)
 
-# Asserts whether a Go executable exists with the specified version.
-#
-# Arguments:
-#   - VERSION: The expected version of the Go executable.
-function(assert_go_executable VERSION)
+section("it should set up the latest version of Go")
+  setup_go()
+
   assert(DEFINED GO_EXECUTABLE)
   assert(EXISTS "${GO_EXECUTABLE}")
+  assert(IS_EXECUTABLE "${GO_EXECUTABLE}")
 
   assert_execute_process(
     COMMAND "${GO_EXECUTABLE}" version
-    OUTPUT "^go version go${VERSION}")
-endfunction()
-
-section("it should set up the latest version of Go")
-  setup_go()
-  assert_go_executable(1.22.5)
+    OUTPUT "^go version go1.22.5")
 endsection()
 
 section("it should set up a specific version of Go")
   setup_go(VERSION 1.21.9)
-  assert_go_executable(1.21.9)
+
+  assert(DEFINED GO_EXECUTABLE)
+  assert(EXISTS "${GO_EXECUTABLE}")
+  assert(IS_EXECUTABLE "${GO_EXECUTABLE}")
+
+  assert_execute_process(
+    COMMAND "${GO_EXECUTABLE}" version
+    OUTPUT "^go version go1.21.9")
 endsection()
