eb59fc6f9451a8db98fbde7c744367fbcf3f4b46
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-31 20:02:58 +0700

    test: utilize `assertion_add_test` to create CMake test

--- test/setup_go.cmake
+++ test/setup_go.cmake
@@ -1,11 +1,3 @@
-cmake_minimum_required(VERSION 3.5)
-
-file(
-  DOWNLOAD https://github.com/threeal/assertion-cmake/releases/download/v1.0.0/Assertion.cmake
-    ${CMAKE_BINARY_DIR}/Assertion.cmake
-  EXPECTED_MD5 1d8ec589d6cc15772581bf77eb3873ff)
-include(${CMAKE_BINARY_DIR}/Assertion.cmake)
-
 find_package(SetupGo REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)
 
 section("it should set up the latest version of Go")
