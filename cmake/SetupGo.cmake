09661d91ccd6278a5639fa160eba01226e0ce8e8
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-01 11:21:55 +0700

    feat: remove include guard in module
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- cmake/SetupGo.cmake
+++ cmake/SetupGo.cmake
@@ -20,8 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-include_guard(GLOBAL)
-
 # Sets up a specific version of Go within this project.
 #
 # This function downloads a specific version of the Go build from the remote server, extracts it,
